021f716e43c597ba4e2559172a6915c364aa1be6
Kai Wells <support@kaiwells.me>
2017-09-01 09:22:31 -0700

    Don't stomp over complex objects

--- assets/js/utility.js
+++ assets/js/utility.js
@@ -3,14 +3,15 @@ function CopyObj(o) {
     return JSON.parse(JSON.stringify(o));
 }
 
-function FisherYatesShuffle(arr) {
+function Shuffle(arr) {
+    // Fisher-Yates (Knuth) Algorithm
     var shuffled = arr.slice(0); // Copy by value
     for (var i = arr.length-1; i > 0; i--) {
         // 1 <= i <= n-1
         var j = Math.floor(Math.random()*(i+1)); // 0 <= j <= i
         // Swap i and j, copying by value
-        var temp = CopyObj(shuffled[i]);
-        shuffled[i] = CopyObj(shuffled[j]);
+        var temp = shuffled[i];
+        shuffled[i] = shuffled[j];
         shuffled[j] = temp;
     }
     return shuffled;
